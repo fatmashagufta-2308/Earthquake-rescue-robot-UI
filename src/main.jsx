d60a38460b63f3f9f5525876e1183675a78b85cd
Fatma Shagufta <fatma.shagufta23@gmail.com>
2026-09-06 14:42:54 +0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ResqCommand from './ResqCommand.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ResqCommand />
+  </React.StrictMode>,
+)
